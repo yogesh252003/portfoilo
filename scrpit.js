305d49523a840adf662b7ffdd615e477e2e3c4f5
yogesh252003 <sherusingh60020@gmail.com>
2024-09-16 11:13:51 +0530

    portfolio

---
+++ scrpit.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "GitHub", "C++", "SQL - Structured Query Language"],
+    typeSpeed: 100,      // Speed of typing
+    backSpeed: 60,       // Speed of backspacing
+    loop: true,          // Enable looping
+});
